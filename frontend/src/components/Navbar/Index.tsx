8e1451174e775910f72f5d08b8b81930c292f693
barbararibeiro <barbararibeiro18@gmail.com>
2022-05-18 11:30:55 -0300

    Alteração

--- frontend/src/components/Navbar/Index.tsx
+++ frontend/src/components/Navbar/Index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                 <a href="https://github.com/barbararibeiroo">
                     <div className='dsmovie-contact-container'>
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/barbararibeiroo</p>
                     </div>
                 </a>
             </div>
